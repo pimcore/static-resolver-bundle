daafa28d32d4bf022b9ab12b97c29bd475ea97b3
Martin Eiber <martin.eiber@pimcore.com>
2023-06-15 16:24:32 +0200

    Initial commit.

---
+++ src/DependencyInjection/Configuration.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Pimcore\Bundle\StaticResolverBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+/**
+ * This is the class that validates and merges configuration from your app/config files.
+ *
+ * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/configuration.html}
+ */
+class Configuration implements ConfigurationInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getConfigTreeBuilder(): TreeBuilder
+    {
+        return new TreeBuilder('pimcore_static_resolver');
+    }
+}
---
+++ src/PimcoreStaticResolverBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Pimcore\Bundle\StaticResolverBundle;
+
+use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
+use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
+use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+
+class PimcoreStaticResolverBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
+{
+    use BundleAdminClassicTrait;
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
+}
