f51e89cd9fc8e28b618a4664b844da6adcc9f454
martineiber <martineiber@users.noreply.github.com>
2023-06-15 14:25:07 +0000

    Apply php-cs-fixer changes

--- src/DependencyInjection/PimcoreStaticResolverExtension.php
+++ src/DependencyInjection/PimcoreStaticResolverExtension.php
@@ -1,11 +1,21 @@
 <?php
 
+/**
+ * Pimcore
+ *
+ * This source file is available under following license:
+ * - Pimcore Commercial License (PCL)
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     PCL
+ */
+
 namespace Pimcore\Bundle\StaticResolverBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
