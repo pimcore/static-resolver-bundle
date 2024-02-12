298b89aa63dde28eeaef621c99323571b8edaa6d
Martin Eiber <martin.eiber@pimcore.com>
2024-02-12 14:43:19 +0100

    Make class internal and final.

--- src/Models/DataObject/FieldCollection/DefinitionResolver.php
+++ src/Models/DataObject/FieldCollection/DefinitionResolver.php
@@ -19,7 +19,10 @@ namespace Pimcore\Bundle\StaticResolverBundle\Models\DataObject\FieldCollection;
 use Exception;
 use Pimcore\Model\DataObject\Fieldcollection\Definition;
 
-class DefinitionResolver implements DefinitionResolverInterface
+/**
+ * @internal
+ */
+final class DefinitionResolver implements DefinitionResolverInterface
 {
     /**
      * @throws Exception
