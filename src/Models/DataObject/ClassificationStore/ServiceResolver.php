b7df200c6d083bb4d4039fb3167cbf8236eec70a
Martin Eiber <martin.eiber@pimcore.com>
2024-02-14 08:04:35 +0100

    Add Classification Store service. (#30)
    * Add Classification Store ServiceResolver.
    
    * Add Classification Store ServiceResolver.
    
    * Code Style.
    
    * Remove static.
    
    * Interface should not be internal.

---
+++ src/Models/DataObject/ClassificationStore/ServiceResolver.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StaticResolverBundle\Models\DataObject\ClassificationStore;
+
+use Exception;
+use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\ClassDefinition\Data\EncryptedField;
+use Pimcore\Model\DataObject\Classificationstore\KeyConfig;
+use Pimcore\Model\DataObject\Classificationstore\KeyGroupRelation;
+use Pimcore\Model\DataObject\Classificationstore\Service;
+
+/**
+ * @internal
+ */
+final class ServiceResolver implements ServiceResolverInterface
+{
+    /**
+     * @throws Exception
+     */
+    public function getFieldDefinitionFromKeyConfig(
+        KeyConfig|KeyGroupRelation $keyConfig
+    ): EncryptedField|Data|null
+    {
+        return Service::getFieldDefinitionFromKeyConfig($keyConfig);
+    }
+}
