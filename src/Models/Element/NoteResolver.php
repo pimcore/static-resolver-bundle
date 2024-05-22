2e943885e6ff1578dab41d1b7b045c39eee67db4
mattamon <mattamon@users.noreply.github.com>
2024-05-22 05:52:39 +0000

    Apply php-cs-fixer changes

--- src/Models/Element/NoteResolver.php
+++ src/Models/Element/NoteResolver.php
@@ -16,7 +16,6 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StaticResolverBundle\Models\Element;
 
-
 use Pimcore\Model\Element\Note;
 
 final class NoteResolver implements NoteResolverInterface
