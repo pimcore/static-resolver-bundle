69236fafbcbbc216c0eae4d4257a4af5d2fcad29
Al3x Zamponi <562324+alexz707@users.noreply.github.com>
2024-06-05 09:38:38 +0200

    Tags resolver (#38)
    * Add Tag resolver
    
    ---------
    
    Co-authored-by: alexz707 <alexz707@users.noreply.github.com>

---
+++ src/Models/Tag/TagResolver.php
@@ -0,0 +1,63 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\StaticResolverBundle\Models\Tag;
+
+use Pimcore\Model\Element\Tag;
+
+class TagResolver implements TagResolverInterface
+{
+    public function getById(int $id): ?Tag
+    {
+        return Tag::getById($id);
+    }
+
+    /**
+     * @return array<int, Tag>
+     */
+    public function getTagsForElement(string $cType, int $cId): array
+    {
+        return Tag::getTagsForElement($cType, $cId);
+    }
+
+    public function assignTagToElement(string $cType, int $cId, Tag $tag): void
+    {
+        Tag::addTagToElement($cType, $cId, $tag);
+    }
+
+    public function unassignTagFromElement(string $cType, int $cId, Tag $tag): void
+    {
+        Tag::removeTagFromElement($cType, $cId, $tag);
+    }
+
+    /**
+     * @param array<int, int> $cIds
+     * @param array<int, int> $tagIds
+     */
+    public function batchAssignTagsToElements(string $cType, array $cIds, array $tagIds): void
+    {
+        Tag::batchAssignTagsToElement($cType, $cIds, $tagIds);
+    }
+
+    /**
+     * @param array<int, int> $cIds
+     * @param array<int, int> $tagIds
+     */
+    public function batchReplaceTagsForElements(string $cType, array $cIds, array $tagIds): void
+    {
+        Tag::batchAssignTagsToElement($cType, $cIds, $tagIds, true);
+    }
+}
