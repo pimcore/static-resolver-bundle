e634a58676c9990aae67e051c18f260f70f034df
martineiber <martineiber@users.noreply.github.com>
2024-06-18 14:30:19 +0000

    Apply php-cs-fixer changes

--- src/Lib/Tools/AdminResolver.php
+++ src/Lib/Tools/AdminResolver.php
@@ -4,11 +4,14 @@ declare(strict_types=1);
 /**
  * Pimcore
  *
- * This source file is available under following license:
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
  * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
- * @license    http://www.pimcore.org/license     PCL
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Bundle\StaticResolverBundle\Lib\Tools;
@@ -21,13 +24,11 @@ use Pimcore\Tool\Admin;
  */
 final class AdminResolver implements AdminResolverInterface
 {
-
     public function reorderWebsiteLanguages(
         User $user,
         array|string $languages,
         bool $returnLanguageArray = false
-    ): array|string
-    {
+    ): array|string {
         return Admin::reorderWebsiteLanguages($user, $languages, $returnLanguageArray);
     }
 }
