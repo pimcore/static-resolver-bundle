d5f40bf72ae109b637a9c1650841eb16e727a87d
Marco Perberschlager <marco.perberschlager@pimcore.com>
2025-01-13 08:31:34 +0100

    Added missing static functions

--- src/Models/Tool/CustomReportResolver.php
+++ src/Models/Tool/CustomReportResolver.php
@@ -16,7 +16,10 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StaticResolverBundle\Models\Tool;
 
+use Pimcore\Bundle\CustomReportsBundle\Tool\Adapter\CustomReportAdapterInterface;
 use Pimcore\Bundle\CustomReportsBundle\Tool\Config;
+use Pimcore\Model\User;
+use stdClass;
 
 /**
  * @internal
@@ -26,5 +29,15 @@ final class CustomReportResolver implements CustomReportResolverInterface
     public function getByName(string $name): ?Config
     {
         return Config::getByName($name);
+    }
+
+    public static function getReportsList(User $user = null): array
+    {
+        return Config::getReportsList($user);
+    }
+
+    public static function getAdapter(?stdClass $configuration, Config $fullConfig = null): CustomReportAdapterInterface
+    {
+        return Config::getAdapter($configuration, $fullConfig);
     }
 }
