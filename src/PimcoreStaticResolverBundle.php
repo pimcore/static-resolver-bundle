e953965836c400830d017e3d23c95bb01a4b4008
mattamon <38959016+mattamon@users.noreply.github.com>
2025-02-25 13:19:59 +0000

    Apply php-cs-fixer changes

--- src/PimcoreStaticResolverBundle.php
+++ src/PimcoreStaticResolverBundle.php
@@ -16,12 +16,12 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StaticResolverBundle;
 
-use Pimcore\Bundle\StaticResolverBundle\DependencyInjection\PimcoreStaticResolverExtension;
-use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use function dirname;
+use Pimcore\Bundle\StaticResolverBundle\DependencyInjection\PimcoreStaticResolverExtension;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
 use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class PimcoreStaticResolverBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
