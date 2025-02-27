0ffd0ccb87c099c6577c6a373a13485a77f57c05
Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
2025-02-27 10:33:04 +0100

    [Task] Add container extension (#99)
    * [Task] Add container extension
    
    * Apply php-cs-fixer changes

--- src/PimcoreStaticResolverBundle.php
+++ src/PimcoreStaticResolverBundle.php
@@ -17,13 +17,20 @@ declare(strict_types=1);
 namespace Pimcore\Bundle\StaticResolverBundle;
 
 use function dirname;
+use Pimcore\Bundle\StaticResolverBundle\DependencyInjection\PimcoreStaticResolverExtension;
 use Pimcore\Extension\Bundle\AbstractPimcoreBundle;
 use Pimcore\Extension\Bundle\PimcoreBundleAdminClassicInterface;
 use Pimcore\Extension\Bundle\Traits\BundleAdminClassicTrait;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 
 class PimcoreStaticResolverBundle extends AbstractPimcoreBundle implements PimcoreBundleAdminClassicInterface
 {
     use BundleAdminClassicTrait;
+
+    public function getContainerExtension(): ExtensionInterface
+    {
+        return new PimcoreStaticResolverExtension();
+    }
 
     public function getPath(): string
     {
