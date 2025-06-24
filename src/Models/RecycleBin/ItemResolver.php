7d13bfa861c03109b120e9fae017e00774486edd
lukmzig <30526586+lukmzig@users.noreply.github.com>
2025-06-24 08:59:55 +0000

    Apply php-cs-fixer changes

--- src/Models/RecycleBin/ItemResolver.php
+++ src/Models/RecycleBin/ItemResolver.php
@@ -13,7 +13,6 @@ declare(strict_types=1);
 
 namespace Pimcore\Bundle\StaticResolverBundle\Models\RecycleBin;
 
-
 use Pimcore\Model\Element\ElementInterface;
 use Pimcore\Model\Element\Recyclebin\Item;
 use Pimcore\Model\User;
