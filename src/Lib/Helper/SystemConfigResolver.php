9675ecc5e92be979d0f189b06eb575b210d6a478
Marco Perberschlager <marco.perberschlager@pimcore.com>
2026-01-15 08:44:14 +0100

    Added SystemConfigResolver

---
+++ src/Lib/Helper/SystemConfigResolver.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * This source file is available under the terms of the
+ * Pimcore Open Core License (POCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (https://www.pimcore.com)
+ *  @license    Pimcore Open Core License (POCL)
+ */
+
+namespace Pimcore\Bundle\StaticResolverBundle\Lib\Helper;
+
+use Pimcore\Helper\SystemConfig;
+use Pimcore\Config\LocationAwareConfigRepository;
+
+/**
+ * @internal
+ */
+final class SystemConfigResolver implements SystemConfigResolverInterface
+{
+    public function getConfigDataByKey(LocationAwareConfigRepository $repository, string $key): array
+    {
+        return SystemConfig::getConfigDataByKey($repository, $key);
+    }
+}
